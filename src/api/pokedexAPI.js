6abc5f99f16fffc7bd87526a22aea993a8ca59cc
Andre <andre.p23@hotmail.com>
2019-01-08 22:12:15 -0800

    Update: now the API_URL is in the config

--- src/api/pokedexAPI.js
+++ src/api/pokedexAPI.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 
+const API_URL= process.env.API_URL;
+
 function getPokemons() {
-    return axios.get('http://localhost:3001/pokemon')
+    return axios.get(API_URL+'/pokemon')
         .then(function (response) {
             const callPokemons = [];
             const listPokemonsCalls = response.data.results;
